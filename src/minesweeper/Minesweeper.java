c574aee9c395feb10888d0d8cdcfd56c60302a85
Davgom16 <2025039@student.cct.ie>
2025-06-23 23:34:52 +0100

    Update Minesweeper.java

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -4,6 +4,9 @@
  */
 package minesweeper;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author dav_g
@@ -14,25 +17,56 @@ public class Minesweeper {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-        int[][] matrix = new int[6][6];
+        
+        Scanner myKB;
+        Random rand;
         int i;
         int j;
-        int count = 1;
+        int h = 5;
+        int count = 0;
+        int user_input = 6;
+        int mine_location;
+        int result;
+        String hit = "x";
+        String miss = "0";
+        String [][] mine_field;
+        int [] op;
+        int row;
+        int col;
         
-        for (i = 0; i < 6; i++) {
+        myKB = new Scanner (System.in);
+        rand = new Random();
+        mine_field = new String[h][h];
+        op = new int [5];
+        mine_location = rand.nextInt(h*h) + 1;
+        result = user_input - mine_location;
+        
+        for (i = 0; i < h; i++) {
             
-            for (j = 0; j < 6; j++) {
+            for (j = 0; j < h; j++) {
               
-                matrix[i][j] = count++;
-                System.out.print(matrix[i][j] + "\t");
+                //matrix[i][j] = String.valueOf(count++);
+                mine_field[i][j] = "-";
+                System.out.print(mine_field[i][j] + "\t");
                 
             }
             
             System.out.println();
             
         } 
-       
+        
+        do {
+            
+            System.out.println("number?");
+            user_input = myKB.nextInt();
+            op[count] = user_input;
+            row = (user_input - 1) / h;
+            col = (user_input - 1) % h;            
+            
+            count = count++;
+            
+        } while (count < 6); 
+        
     }
     
 }
