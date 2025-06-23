b98c7c08245011233d0d5053a642658a43d755f1
Davgom16 <2025039@student.cct.ie>
2025-06-23 13:44:54 +0100

    creating the matrix

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -14,7 +14,25 @@ public class Minesweeper {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        int[][] matrix = new int[6][6];
+        int i;
+        int j;
+        int count = 1;
+        
+        for (i = 0; i < 6; i++) {
+            
+            for (j = 0; j < 6; j++) {
+              
+                matrix[i][j] = count++;
+                System.out.print(matrix[i][j] + "\t");
+                
+            }
+            
+            System.out.println();
+            
+        } 
+       
     }
     
 }
